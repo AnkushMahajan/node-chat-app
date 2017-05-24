68d3b859eda43e65837ea785f4aeb5a38632bf2b
Ankush Mahajan <vish_sukhoi@yahoo.co.in>
2017-05-24 11:41:17 +1000

    connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
 app.use(express.static(publicPath));
 
-app.listen('3000', () => {
+let server = http.createServer(app);
+
+// websocket server
+let io = socketIO(server);
+
+// event handler
+io.on('connection', (socket) => {
+    console.log('New user connected');
+    socket.on('disconnect', (socket) => {
+        console.log('User disconnected');
+    })
+});
+
+
+
+server.listen('3000', () => {
     console.log('App is listening on port 3000');
 })
